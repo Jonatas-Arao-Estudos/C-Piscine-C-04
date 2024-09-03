8169b027c5de99411b4193df0dd404c7feed71e8
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-09-03 18:57:24 -0300

    ex03 added

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 14:37:14 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/09/03 14:50:29 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	num;
+	int	index;
+	int	sign;
+
+	index = 0;
+	num = 0;
+	sign = 1;
+	while (str[index] == ' ' || str[index] == '\f' || str[index] == '\n'
+		|| str[index] == '\r' || str[index] == '\t' || str[index] == '\v')
+		index = index + 1;
+	while (str[index] == '+' || str[index] == '-')
+	{
+		if (str[index] == '-')
+			sign = sign * (-1);
+		index = index + 1;
+	}
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		num = (str[index] - '0') + num * 10;
+		index = index + 1;
+	}
+	return (num * sign);
+}
